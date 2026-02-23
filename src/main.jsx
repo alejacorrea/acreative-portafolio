bd3628f548d828863ce587405cc4c1d03ae1aa0d
alejacorrea <alejandracp63@gmail.com>
2026-02-23 16:54:44 -0500

    bloquear clic derecho imagenes

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,13 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
+// Bloquea clic derecho en imágenes
+document.addEventListener('contextmenu', (e) => {
+  if (e.target.tagName === 'IMG') {
+    e.preventDefault();
+  }
+});
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
